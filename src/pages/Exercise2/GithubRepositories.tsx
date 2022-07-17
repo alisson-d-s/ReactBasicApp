40268c1034075adb1c198cbf540ed9e8c78f6477
Alisson <Alisson@DESKTOP-P5MO7DJ>
2022-07-16 22:18:08 -0300

    GithutRepositories list

--- src/pages/Exercise2/GithubRepositories.tsx
+++ src/pages/Exercise2/GithubRepositories.tsx
@@ -1,21 +1,63 @@
-import { useEffect, useState } from "react";
-import { RepositoriesList, RepositoriesListProps } from "./RepositoriesList";
+import React, { useEffect, useState } from "react";
+import { RepositoriesList } from "./RepositoriesList";
+
 import "../../css/GithubRepositories.css";
 
-
 export const GithubRepositories = () => {
-    const [repo, setRepo] = useState<RepositoriesListProps>();
+    const [link, setLink] = useState<string>("");
+    const [username, setUsername] = useState("");
+    const [repo, setRepo] = useState([]);
+    const [errorMessage, setErrorMessage] = useState("");
 
     useEffect(() => {
-        fetch("https://api.github.com/users/alisson-d-s/repos")
-        .then(async response => setRepo(await response.json()));
-    }, []);
+        setLink(`https://api.github.com/users/${username}/repos`);
+    }, [username]);
 
-    if (!repo) {
-        return <h1>Aguarde...</h1>
-    }
+    const handleUserNameChange = (value: string) => {
+        setUsername(value);
+    };
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        if (username === "") {
+            setErrorMessage("Username are empty!");
+            setRepo([]);
+            return;
+        }
+
+        const response = await fetch(link, {
+            method: "GET"
+        });
+
+        if (!response.ok) {
+            setErrorMessage(response.statusText);
+            setRepo([]);
+            return;
+        }
+
+        setErrorMessage("");
+        setRepo(await response.json());
+    };
 
     return (
-        <RepositoriesList repositories={repo} />
+        <div>
+            <form onSubmit={(e) => handleSubmit(e)} >
+                <div className="container" >
+                    <input 
+                        placeholder="GitHub username" 
+                        key={"userName"} type={"text"} 
+                        className="input-repo-link" 
+                        value={username} 
+                        onChange={(e) => handleUserNameChange(e.currentTarget.value)} 
+                    />
+                    <input value={"Search"} type={"submit"} className="button" />
+                </div>
+                <span className="container">Link: {link}</span>
+                <span className="container" >{errorMessage}</span>
+            </form>
+            <div>
+                <RepositoriesList repositories={repo} />          
+            </div>
+        </div>
     );
 };
